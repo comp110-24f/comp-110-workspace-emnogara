2cc672280df6336037562bf6744a342ec37b0a8f
emnogara <emnogara@unc.edu>
2024-09-16 12:42:35 -0400

    progress for ex02

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -1,1 +1,13 @@
+"""EX02 - Chardle - A cute step toward Wordle."""
 
+__author__ = "730670853"
+
+
+def input_word() -> str:
+    """Player will input a word into the game."""
+    user_word: str = input("Enter a 5-character word: ")
+    if len(user_word) != 5:
+        print("Error: Word must contain 5 characters.")
+
+
+input_word()
